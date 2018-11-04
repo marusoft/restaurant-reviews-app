2ee111dcf6352fa37b129ef78eff1c78ceea64d5
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2018-11-04 11:19:18 +0100

    add install and fetch event for the cached files

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 var staticCacheName = 'restaurant-reviews-app';
 
+// The install event listener opens the caches object 
+// and then populates it with the list of resources that we want to cache.
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open('airhorner').then(function (cache) {
@@ -30,3 +32,13 @@ self.addEventListener('install', function (e) {
         }).then(console.log('Cache is sucessful!'))
     );
 });
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+ 
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+ });
