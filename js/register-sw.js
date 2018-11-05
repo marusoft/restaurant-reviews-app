6f775501d442f75db6db1edb72a21f3a5977b640
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2018-11-05 11:38:50 +0100

    fix bug

--- js/register-sw.js
+++ js/register-sw.js
@@ -6,3 +6,25 @@ if ('serviceWorker' in navigator) {
             console.log("Service Worker Registered");
         });
 }
+
+
+// This script checks if the browser supports service workers
+//   if ('serviceWorker' in navigator) {
+//       window.addEventListener('load', () => {
+//           navigator.serviceWorker
+//           .register('/sw.js')
+//           .then((res) => {
+//               console.log(res);
+//               console.log("Service Worker Registered");
+//           })
+//           .catch((err) => {
+//               console.log(err);
+//               console.log("Couldn't register service worker...");
+//           })
+//       });
+//   }
+
+//   window.addEventListener('beforeinstallprompt', (e) => {
+//       // display prompt
+//       e.prompt();
+//   });
